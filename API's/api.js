628f2de1edf1412d44ca5f48377620ccad3da565
NoahDcode <noahduran08@gmail.com>
2022-03-31 08:43:43 -1000

    submit again

---
+++ API's/api.js
@@ -0,0 +1,9 @@
+//Fetch Function
+
+fetch("URL TO make removeEventListenequest to")
+    .then(res => console.log(res))  //successful response
+    .then()
+    .catch(err => console.log(err)) // unseccusful response just incase server shut down for some reason
+
+
+
